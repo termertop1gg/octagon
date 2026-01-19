4d0e5f4c9f74b554721e1e19fe075edd02901fc7
Alex <pazdnekovsasha@gmail.com>
2026-01-19 18:52:31 +0800

    Add static and dynamic routes

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ app.get('/', (req, res) => {
   res.send('<h1>Привет, Октагон!</h1>');
 });
 
+app.get('/static', (req, res) => {
+  res.json({
+    header: "Hello",
+    body: "Octagon NodeJS Test"
+  });
+});
+
+app.get('/dynamic', (req, res) => {
+  let { a, b, c } = req.query;
+
+  if (!a || !b || !c || isNaN(a) || isNaN(b) || isNaN(c)) {
+    return res.json({ header: "Error" });
+  }
+
+  a = Number(a);
+  b = Number(b);
+  c = Number(c);
+  
+  const result = (a * b * c) / 3;
+
+  res.json({
+    header: "Calculated",
+    body: String(result)
+  });
+});
+
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`);
 });
