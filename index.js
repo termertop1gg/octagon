d95cfe057d698bc6ad855ef9d647ad794397933c
Alex <pazdnekovsasha@gmail.com>
2026-01-19 20:56:51 +0800

    Create basic Telegram bot

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Привет, Октагон!</h1>');
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TELEGRAM_TOKEN;
+
+if (!token) {
+    console.error('Ошибка: Токен не найден! Проверьте файл .env');
+    process.exit(1);
+}
+
+const bot = new TelegramBot(token, {polling: true});
+
+console.log('Бот запущен...');
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Привет, октагон!");
 });
 
-app.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`);
-});
+bot.on("polling_error", console.log);
