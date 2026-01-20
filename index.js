3337a312344a894f37bc7597cbe9f80219187cf6
Alex <pazdnekovsasha@gmail.com>
2026-01-20 20:23:02 +0800

    Added commands: help, site, creator

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 require('dotenv').config();
-
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TELEGRAM_TOKEN;
 
 if (!token) {
-    console.error('Ошибка: Токен не найден! Проверьте файл .env');
+    console.error('Ошибка: Не найден токен в файле .env');
     process.exit(1);
 }
 
 const bot = new TelegramBot(token, {polling: true});
 
-console.log('Бот запущен...');
+console.log('Бот запущен и ждет команд...');
+
+
+
+bot.onText(/\/help/, (msg) => {
+    const chatId = msg.chat.id;
+    const text = `
+Вот список доступных команд:
+/help - Показать это сообщение
+/site - Ссылка на сайт Октагона
+/creator - Узнать, кто мой создатель
+    `;
+    bot.sendMessage(chatId, text);
+});
+
+bot.onText(/\/site/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Сайт школы: https://octagon-students.ru/");
+});
+
+bot.onText(/\/creator/, (msg) => {
+    const chatId = msg.chat.id;
+    const myName = "Паздников Алексчандр Олегович"; 
+    bot.sendMessage(chatId, `Мой создатель: ${myName}`);
+});
+
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, "Привет, октагон!");
+    bot.sendMessage(chatId, "Привет, октагон! Напиши /help, чтобы узнать, что я умею.");
 });
-
-bot.on("polling_error", console.log);
